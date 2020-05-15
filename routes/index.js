2ee9efcd79fd527b598f1d90437903c8e0e1e264
Matthew Abrahamsen <muchohot22@gmail.comm>
2020-05-14 21:30:05 -0400

    added my image

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Matthew Abrahamsen' });
+  res.render('index', { title: 'Matthew' });
 });
 
 module.exports = router;
